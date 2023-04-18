6bc77a8227dc974ed059db34dcb78939b01110d8
asmita <asmitampawar@gmail.com>
2023-04-18 16:12:01 +0530

    route array

---
+++ RoutingDemo/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TechnologyComponent } from './technology/technology.component';
+import { BooksComponent } from './books/books.component';
+
+const routes: Routes = [
+
+  {path:'technology',component:TechnologyComponent},
+  {path:'books',component:BooksComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
